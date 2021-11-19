75edd663e942b79e12bf23eba809ddea841fbade
2021 DSM43 Duarte V Fernando <al222010329@gmail.com>
2021-11-18 19:00:22 -0600

    Hicimos que retornara la api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductoController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::resource('productos', ProductoController::class);
