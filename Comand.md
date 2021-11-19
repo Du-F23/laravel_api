29857f2e2c60f260e9f5639773ae1baf1bbd82df
2021 DSM43 Duarte V Fernando <al222010329@gmail.com>
2021-11-18 18:14:51 -0600

    Creamos la estructura de la tabla producto

--- Comand.md
+++ Comand.md
@@ -1,3 +1,7 @@
 ## Crear Modelos en Laravel
 
 php artisan make:model Producto --all
+
+## Creamos las tablas
+
+php artisan migrate:fresh
--- database/migrations/2021_11_19_000147_create_productos_table.php
+++ database/migrations/2021_11_19_000147_create_productos_table.php
@@ -15,6 +15,13 @@ class CreateProductosTable extends Migration
     {
         Schema::create('productos', function (Blueprint $table) {
             $table->id();
+            $table->string('producto', 45);
+            $table->unsignedInteger('existencias');
+            $table->decimal('precio', 10, 2);
+            $table->string('fotografia');
+            $table->tinyText('descripcion');
+            $table->enum('estatus',['Activo', 'Inactivo']);
+            $table->decimal('iva', 10, 2);
             $table->timestamps();
         });
     }
